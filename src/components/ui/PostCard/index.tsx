e4fda83a8004f9c8b2e6e28e22b569f65cfc1d02
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2024-01-17 01:03:44 +0300

    feat: add PostCard ui component

---
+++ src/components/ui/PostCard/index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import style from '@styles/ui/postCard.module.scss';
+
+export interface IPostCard{
+  imageSrc: string;
+  tags: string[];
+  title: string;
+  description: string;
+}
+export default function PostCard({imageSrc, tags, title, description}: IPostCard) {
+  return (
+    <article className={style.postCardArticle}>
+      <Image src={imageSrc} alt='post image' width={490} height={318}/>
+      <div className={style.infoWrapper}>
+        <p className={style.tags}>{tags.join(' ')}</p>
+        <h2 className={style.title}>{title}</h2>
+        <p className={style.description}>{description}</p>
+      </div>
+    </article>
+  )
+}
