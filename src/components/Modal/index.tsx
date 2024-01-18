1f8e4f31a23c04079a612b30d8ee9ccd8e31ec9e
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2024-01-18 21:29:51 +0300

    feat: add modal with video

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,21 @@
+
+import { IModal } from '@customTypes/modal';
+import Image from 'next/image';
+import style from '@styles/modal/modal.module.scss'
+
+import { Portal } from '../Portal';
+
+export function Modal({ onClose, children }: IModal) {
+  return (
+    <Portal>
+      <div className={style.wrapper}>
+        <div className={style.content}>
+          {children}
+          <button type='button' className={style.close} onClick={onClose}>
+            <Image src='/icons/close.svg' alt='close' width={28} height={28}/>
+          </button>
+        </div>
+      </div>
+    </Portal>
+  );
+}
--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,9 +1,13 @@
+
+
 import style from '@styles/ui/header.module.scss';
-import button from '@styles/ui/button.module.scss';
 import { LocaleType } from '@customTypes/locale';
+
+
 
 import { CONSTANTS } from './constants';
 import LocaleSwitcher from './LocaleSwitcher';
+import OpenModal from './components/OpenModal';
 
 import { Navigation } from '../Navigation';
 
@@ -12,6 +16,7 @@ import { getDictionary } from '@/lib/dictionary';
 export default async function Header({ lang }: LocaleType) {
   const { navigation, stepByStep } = await getDictionary(lang);
   const nameOfLinks = Object.values(navigation);
+  
 
   return (
     <header className={style.header}>
@@ -19,10 +24,9 @@ export default async function Header({ lang }: LocaleType) {
       <LocaleSwitcher />
       <div className={style.wrapper}>
         <Navigation type='header' nameOfLinks={nameOfLinks} />
-        <button type='button' className={button.secondary}>
-          {stepByStep.btn}
-        </button>
+        <OpenModal btnText={stepByStep.btn}/>
       </div>
+      
     </header>
   );
 }
