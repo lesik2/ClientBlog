a97a53fafb7600ebb2a86ae82ae13f5601fcc3ec
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2024-01-27 17:40:17 +0300

    fix: update error boundary

--- src/app/[lang]/error.tsx
+++ src/app/[lang]/error.tsx
@@ -10,7 +10,7 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
   return (
     <div>
       <h2>Something went wrong!</h2>
-      <button type='button' onClick={() => reset()}>
+      <button type='button' onClick={reset}>
         Try again
       </button>
     </div>
