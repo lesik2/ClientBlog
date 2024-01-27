bad366eece446de03e7de2172950a1ddf32c6a41
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2024-01-27 18:18:58 +0300

    fix: add useMemo

--- src/app/[lang]/blog/[id]/components/Posts/index.tsx
+++ src/app/[lang]/blog/[id]/components/Posts/index.tsx
@@ -1,18 +1,19 @@
 import { POST_CARDS } from '@constants/cards';
+import { memo } from 'react';
 
 import style from './posts.module.scss';
 
 import PostCard from '../PostCard';
 
-export default function Posts({ category }: { category: string }) {
-  return (
-    <section className={style.postsSection}>
-      <h2 className={style.title}>What to read next</h2>
-      <div className={style.wrapperPosts}>
-        {POST_CARDS.filter((card) => card.category === category).map((card) => (
-          <PostCard key={card.id} {...card} />
-        ))}
-      </div>
-    </section>
-  );
-}
+const Posts = memo(({ category }: { category: string }) => (
+  <section className={style.postsSection}>
+    <h2 className={style.title}>What to read next</h2>
+    <div className={style.wrapperPosts}>
+      {POST_CARDS.filter((card) => card.category === category).map((card) => (
+        <PostCard key={card.id} {...card} />
+      ))}
+    </div>
+  </section>
+));
+
+export default Posts;
